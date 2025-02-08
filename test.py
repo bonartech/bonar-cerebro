d901343ea2241c0078f14a81a86f2abeed3f452c
lucianoigit <lucianoiriartegit@gmail.com>
2025-02-08 11:39:52 -0300

    first commit

---
+++ test.py
@@ -0,0 +1,15 @@
+from cerebro import encadenamiento_de_pensamiento, mostrar_grafo, procesar_texto_y_construir_grafo
+
+print('test el leon es un mamifero grande')
+procesar_texto_y_construir_grafo("El león es un mamífero grande.")
+mostrar_grafo()
+
+print("test tigre", "león")
+encadenamiento_de_pensamiento("tigre", "león")
+mostrar_grafo()
+
+
+print('test El sol es una estrella brillante en el cielo')
+procesar_texto_y_construir_grafo("El sol es una estrella brillante en el cielo.")
+mostrar_grafo()
+encadenamiento_de_pensamiento("sol", "mamífero")
